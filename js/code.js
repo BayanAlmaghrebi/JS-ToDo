f797489b037c73eb96939fca83ca3cabb501f5dd
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-06-14 23:55:13 +0200

    add JS Input and Btn

--- js/code.js
+++ js/code.js
@@ -1,1 +1,10 @@
-// coding
+console.log('welcome')
+
+const TodoInput = document.querySelector('.todo-input')
+const TodoBtn = document.querySelector('.todo-btn')
+
+function addTodo(){
+    console.log('--------------')
+}
+
+TodoBtn.addEventListener('click',addTodo)
