d8ed8e0a631f54a86c647473089bfcdb43b80ab2
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-06-15 22:06:05 +0200

    add new task

--- js/code.js
+++ js/code.js
@@ -13,10 +13,29 @@ function addTodo(e){
 
     const todoTitle = document.createElement('h4')
     todoTitle.innerText = TodoInput.value
+    TodoInput.value = ''
     todoTitle.classList.add('d-inline-block')
     todoTitle.classList.add('me-5')
-
     todoLi.appendChild(todoTitle)
+
+    const todoAction = document.createElement('div')
+    todoAction.classList.add('d-inline-block')
+    todoAction.classList.add('float-end')
+
+    const btnComplete = document.createElement('button')
+    btnComplete.classList.add('btn')
+    btnComplete.classList.add('btn-primary')
+    btnComplete.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todoAction.appendChild(btnComplete)
+
+
+    const btnDelete = document.createElement('button')
+    btnDelete.classList.add('btn')
+    btnDelete.classList.add('btn-danger')
+    btnDelete.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    todoAction.appendChild(btnDelete)
+
+    todoLi.appendChild(todoAction)
     TodoResult.appendChild(todoLi)
 }
 
